3852b20f91193507d3d2099ee65f0161f83f9c2c
zhaoxu <18901842235@163.com>
2024-04-14 00:51:12 +0800

    提交

---
+++ basicgrammar/src/main/java/com/serendipity/myold/aop/cglib/CglibDemo01.java
@@ -0,0 +1,50 @@
+package com.serendipity.myold.aop.cglib;
+
+
+import org.junit.Test;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * 使用Cglib实现动态代理
+ */
+public class CglibDemo01 {
+
+    public class UserServiceImpl {
+        void play() {
+            System.out.println("Cglib UserServiceImpl play");
+        }
+    }
+
+    public class CglibProxy implements MethodInterceptor {
+
+        private Enhancer enhancer = new Enhancer();
+
+        public Object getProxy(Class clazz) {
+            enhancer.setSuperclass(clazz);
+            enhancer.setCallback(this);
+            return enhancer.create();
+        }
+
+
+        @Override
+        public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+            System.out.println("before.");
+            Object invoke = methodProxy.invokeSuper(o, objects);
+            System.out.println("after.");
+            return invoke;
+        }
+    }
+
+
+    @Test
+    public void test() {
+        CglibProxy cglibProxy = new CglibProxy();
+        UserServiceImpl proxy = (UserServiceImpl) cglibProxy.getProxy(UserServiceImpl.class);
+        proxy.play();
+    }
+
+}
