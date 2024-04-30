59f1fdbf3730494e97e0d2630725caa87dd4207f
zhaoxu <1193131227@qq.com>
2024-04-30 21:47:35 +0800

    commit

--- basicgrammar/src/main/java/com/serendipity/myold/collection/ListDemo.java
+++ basicgrammar/src/main/java/com/serendipity/myold/collection/ListDemo.java
@@ -1,8 +1,15 @@
 package com.serendipity.myold.collection;
 
+import com.github.jsonzou.jmockdata.JMockData;
+import com.github.jsonzou.jmockdata.MockConfig;
+import com.github.jsonzou.jmockdata.TypeReference;
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * ClassName ListDemo
@@ -30,4 +37,33 @@ public class ListDemo {
 
     }
 
+    /**
+     * removeIf
+     */
+    @Test
+    public void testRemoveIf() {
+        // List<String> mock = JMockData.mock(new TypeReference<List<String>>() {});
+        List<String> mockList = Lists.newArrayList("a", "b", "c");
+        System.out.println(mockList);
+        mockList.removeIf(s -> s.equals("a"));
+        System.out.println(mockList);
+
+        List<String> b = mockList.stream().filter(s -> !s.equals("b")).toList();
+        System.out.println(b);
+    }
+
+
+    /**
+     * subList
+     */
+    @Test
+    public void testSubList() {
+        ArrayList<@Nullable Object> objects = Lists.newArrayList();
+        // 匿名内部类
+        ArrayList<String> strings = new ArrayList<>() {{
+            add("a");
+            add("b");
+            add("c");
+        }};
+    }
 }
